fa033aa3ec4aced812d440066060b4d3a59677ae
rohanrao35 <31747010+rohanrao35@users.noreply.github.com>
2018-02-02 14:19:08 -0500

    Setup

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 var express = require('express');
-var app = require();
+var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 //Connect to mongoose
-//mongoose.connect('mongodb://rohanrao35:e@ds121588.mlab.com:21588/encryptedx2')
-mongoose.connect('mongodb://localhost/')
+mongoose.connect('mongodb://rohanrao35:fitracker@ds121588.mlab.com:21588/encryptedx2')
+//mongoose.connect('mongodb://localhost/')
+//mongodb://<dbuser>:<dbpassword>@ds121588.mlab.com:21588/encryptedx2
+//mongoose.connect('mongodb://rohanrao35:fitracker1@ds129946.mlab.com:29946/fitracker');
 var db = mongoose.connection;
-
+User =require('./models/user');
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello ');
 });
+
+
+
+
+app.listen(3000);
+
+
+app.get('/users', function(req, res){
+
+  User.getUsers(function(err, users){
+      if(err){
+        throw err;
+      }
+      res.json(users);
+  });
+});
+
+
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+
+
+var UserSchema = new mongoose.Schema({
+
+  firstName: String,
+	lastName: String,
+  email: String,
+  password: String,
+  accessKey: Number,
+  securityQuestion: String,
+  securityAnswer: String,
+  files: Array,
+  sharedWith: Array,
+  sharing: Array
+});
+
+
+const UserModel = mongoose.model('User', UserSchema);
+module.exports = UserModel;
+
+module.exports.getUsers = (callback) => {
+	User.find(callback);
+}
+
+
+module.exports.addUser = (user, callback) => {
+	User.create(user, callback);
+}
