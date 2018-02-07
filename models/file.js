7e9d8e12a59cb76459693eec2f969d9285efad50
Revanth <rnagurla@purdue.edu>
2018-02-06 21:09:08 -0500

    file model

---
+++ models/file.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+
+var FileSchema = new mongoose.Schema({
+
+  owner: String,
+  title: String,
+  link: String,
+  comments: String,
+  dateCreated: String,
+  usersShared: Array,
+  timeToDelete: Number,
+  timeoutOption: Number
+});
+
+const FileModel = mongoose.model('Files', FileSchema);
+module.exports = FileModel;
+
+module.exports.getFiles = (callback) => {
+	Files.find(callback);
+}
+
+
+module.exports.removeFile = (id, callback) => {
+  var query = { link: _link };
+  console.log(query);
+	Files.remove(query, callback);
+}
+
+module.exports.addFile = (file, callback) => {
+
+	Files.create(file, callback);
+}
